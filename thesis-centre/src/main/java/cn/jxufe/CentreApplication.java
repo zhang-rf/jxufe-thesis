a9250bdc7295525e64c019b7d57442fdb2b67d4c
zhangrongfan <ljs123456>
2016-12-12 15:48:46 +0800

    Initial commit

---
+++ thesis-centre/src/main/java/cn/jxufe/CentreApplication.java
@@ -0,0 +1,12 @@
+package cn.jxufe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentreApplication.class, args);
+	}
+}
