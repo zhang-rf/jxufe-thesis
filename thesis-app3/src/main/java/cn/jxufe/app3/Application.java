c9fff4b686e9c00853c71760d5179ab3a84d7aef
zhangrongfan <ljs123456>
2016-12-13 15:07:05 +0800

    1.0-RELEASE

--- thesis-app3/src/main/java/cn/jxufe/app3/Application.java
+++ thesis-app3/src/main/java/cn/jxufe/app3/Application.java
@@ -13,14 +13,14 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource("classpath:dubbo.xml")
 public class Application {
 
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 
         Service1 service1 = applicationContext.getBean(Service1.class);
-        logger.info(service1.invoke());
+        LOGGER.info(service1.invoke());
         Service2 service2 = applicationContext.getBean(Service2.class);
-        logger.info(service2.invoke());
+        LOGGER.info(service2.invoke());
     }
 }
